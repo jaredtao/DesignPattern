1fa5736137cfa02238086d083b536082c2a89f93
jiawentao <wentaojia2014@163.com>
2018-09-30 17:13:06 +0800

    update ListVisitor

--- code/Visitor/ListVisitor.h
+++ code/Visitor/ListVisitor.h
@@ -9,11 +9,11 @@ class ListVisitor : public Visitor
 public:
     virtual void visit(File *file) override 
     {
-        std::cout << m_currentDir  << "/" << file->getName() << std::endl;
+        std::cout << m_currentDir  << "/" << file->getName()  << " " << file->getSize() << std::endl;
     }
     virtual void visit(Directory *dir) override
     {
-        std::cout << m_currentDir  << "/" << dir->getName() << std::endl;
+        std::cout << m_currentDir  << "/" << dir->getName() << " " << dir->getSize() << std::endl;
         std::string saved = m_currentDir; 
         m_currentDir += "/" + dir->getName();
         auto entryList = dir->getEntryList();
